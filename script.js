1eec7d7c07cbd71e1a17a7014e074a29e70b5401
Peter <pdnguyen714@gmail.com>
2020-06-19 15:50:58 -0700

    fixed isValidEmail function

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValid(email) {
+function isValidEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
@@ -36,6 +36,8 @@ form.addEventListener('submit', function(e) {
     
     if(email.value === '') {
         showError(email, 'Email is required');
+    } else if(!isValidEmail(email.value)){
+        showError(email, 'Email is not valid')
     } else {
         showSuccess(email);
     }
