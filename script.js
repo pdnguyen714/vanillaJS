5e72e590720f00f9d56d3efb72003519d2c28f3e
Peter <pdnguyen714@gmail.com>
2020-06-19 16:05:35 -0700

    created getFieldName function to capitalize first letter of required fields

--- script.js
+++ script.js
@@ -28,11 +28,16 @@ function isValidEmail(email) {
 function checkRequired(inputArr) {
     inputArr.forEach(function(input) {
         if(input.value.trim() === '') {
-            showError(input, 'is required');
+            showError(input, `${getFieldName(input)} is required`);
         } else {
             showSuccess(input);
         }
     });
+}
+
+// Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Even Listeners
