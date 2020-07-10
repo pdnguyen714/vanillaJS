4e078ce901c366d14852fe66d2a83d75e4980714
Peter <pdnguyen714@gmail.com>
2020-07-10 15:03:27 -0700

    created returnACharacter function that returns index of the string

--- hr-practice/script.js
+++ hr-practice/script.js
@@ -4,9 +4,15 @@
 // target.style.background = "black";
 // target.style.color = "white";
 
-var testString = 'words';
+// var testString = 'words';
 
-var firstCharacter = testString[0]; // accesses the first character: 'w'
-var thirdCharacter = testString[2]; // accesses the third character: 'r'
-console.log(firstCharacter); // will log 'w'
-console.log(thirdCharacter); // will log 'r'
+// var firstCharacter = testString[0]; // accesses the first character: 'w'
+// var thirdCharacter = testString[2]; // accesses the third character: 'r'
+// console.log(firstCharacter); // will log 'w'
+// console.log(thirdCharacter); // will log 'r'
+
+function returnACharacter(string, index) {
+    return string[index];
+  }
+  var character = returnACharacter('tea', 1);
+  console.log('character:', character);
