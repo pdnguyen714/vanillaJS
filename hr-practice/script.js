f92dbb9621da76b96fb596d8728f2f161fb7a71b
Peter <pdnguyen714@gmail.com>
2020-07-10 14:59:06 -0700

    implemented test of finding index of elements in vanilla JS

--- hr-practice/script.js
+++ hr-practice/script.js
@@ -1,5 +1,12 @@
-var elements = document.getElementsByClassName("title");
-var target = elements[1];
-target.innerHTML = "DISCOVER YOUR INNER HACKER";
-target.style.background = "black";
-target.style.color = "white";
+// var elements = document.getElementsByClassName("title");
+// var target = elements[1];
+// target.innerHTML = "DISCOVER YOUR INNER HACKER";
+// target.style.background = "black";
+// target.style.color = "white";
+
+var testString = 'words';
+
+var firstCharacter = testString[0]; // accesses the first character: 'w'
+var thirdCharacter = testString[2]; // accesses the third character: 'r'
+console.log(firstCharacter); // will log 'w'
+console.log(thirdCharacter); // will log 'r'
