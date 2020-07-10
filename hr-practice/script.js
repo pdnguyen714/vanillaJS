50590583f91c245012fda328ea51515d0d3d7a0e
Peter <pdnguyen714@gmail.com>
2020-07-10 14:46:27 -0700

    created hr-practice directory and implemented script.js

---
+++ hr-practice/script.js
@@ -0,0 +1,5 @@
+var elements = document.getElementsByClassName("title");
+var target = elements[1];
+target.innerHTML = "DISCOVER YOUR INNER HACKER";
+target.style.background = "black";
+target.style.color = "white";
--- tlm/script.js
+++ tlm/script.js
@@ -14,3 +14,27 @@ function wordsReverser(string){
     }
     
     console.log(wordsReverser('Hello Indiana Class!'));
+
+    function btnSearch(btnName, res){
+        mongoose.connect('mongodb://localhost/Recipes', function(err, db){
+            db.collection('Recipes').find({'recipe.category':btnName}).sort({_id:-1}).toArray(function(err,doc){
+                if(doc.length>0){
+                    let createdBy= [];
+                    let recipeInfo = [];
+                    doc.forEach(recipeArr => {
+                        createdBy.push(recipeArr.createdBy);
+                        recipeArr.recipe.forEach(info => {
+                            if(info.category === btnName){
+                                recipeInfo.push(info);
+                            }
+                        });
+                    });
+                    res.render('btnSearch',{createdBy, recipeInfo});
+                }else{
+                    let zero ="Sorry we don't have any recipes for this category yet! Feel free to add your own!";
+                    res.render('index',{zero});
+                }
+                db.close()
+            });
+        });
+    }    
