88c0a19a868d43868a61eb345aa24d09b3c8bb8b
Peter <pdnguyen714@gmail.com>
2020-07-10 15:30:07 -0700

    implemented returnAValue func with objs

--- hr-practice/script.js
+++ hr-practice/script.js
@@ -1,8 +1,12 @@
+// ========== targeting elements practice ==========
+
 // var elements = document.getElementsByClassName("title");
 // var target = elements[1];
 // target.innerHTML = "DISCOVER YOUR INNER HACKER";
 // target.style.background = "black";
 // target.style.color = "white";
+
+// ========== index practice ==========
 
 // var testString = 'words';
 
@@ -11,8 +15,35 @@
 // console.log(firstCharacter); // will log 'w'
 // console.log(thirdCharacter); // will log 'r'
 
-function returnACharacter(string, index) {
-    return string[index];
+// ========== function practice ==========
+
+// function returnACharacter(string, index) {
+//     return string[index];
+//   }
+//   var character = returnACharacter('tea', 1);
+//   console.log('character:', character);
+
+// ========== arrays practice ==========
+// function returnAnElement(array, index) {
+//     return array[index];
+//   }
+  
+//   var scores = [98, 85, 91, 78, 82];
+//   var position = 3;
+//   var positionElement = returnAnElement(scores, position);
+//   console.log('positionElement:', positionElement);
+  
+  // ========== objects practice ==========
+  function returnAValue(obj, key) {
+    return obj[key];
   }
-  var character = returnACharacter('tea', 1);
-  console.log('character:', character);
+  
+  var computer = {
+    name: "MacBook Pro",
+    version: "10.14.6",
+    processor: "2 GHz Intel Core i7",
+    memory_gb: 8
+  };
+  var nameOfKey = "version";
+  var valueAtKey = returnAValue(computer, nameOfKey);
+  console.log('valueAtKey:', valueAtKey);
