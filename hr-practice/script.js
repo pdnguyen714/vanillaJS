caaaabf85d6ccf071485ff6a78bafe1b98518497
Peter <pdnguyen714@gmail.com>
2020-07-20 15:50:09 -0700

    Numbers practice with vanilla JS

--- hr-practice/script.js
+++ hr-practice/script.js
@@ -48,9 +48,103 @@
 //   var valueAtKey = returnAValue(computer, nameOfKey);
 //   console.log('valueAtKey:', valueAtKey);
 
-  // ========== booleans practice ==========
-  var haveACar = true;
-  var bussesAreRunning = true;
-  
-  var willAttendEvent = haveACar || bussesAreRunning;
-  console.log("I will attend the event:", willAttendEvent);
+// ========== booleans practice ==========
+// var haveACar = true;
+// var bussesAreRunning = true;
+
+// var willAttendEvent = haveACar || bussesAreRunning;
+// console.log("I will attend the event:", willAttendEvent);
+
+// ========== numbers practice ==========
+function getSum(num1, num2) {
+  // create a result variable,
+  var result;
+  // assign it to num1 and num2 added together
+  var result = num1 + num2;
+  // return the result variable
+  return result;
+}
+
+function getDifference(num1, num2) {
+  // create a result variable,
+  var result;
+  // assign it to num1 minus num2
+  result = num1 - num2;
+  // return the result variable
+  return result;
+}
+
+function getProduct(num1, num2) {
+  // create a result variable,
+  var result;
+  // assign it to num1 times num2
+  result = num1 * num2;
+  // return the result variable
+  return result;
+}
+
+function getQuotient(num1, num2) {
+  // create a result variable,
+  var result;
+  // assign it to num1 divided by num2
+  result = num1 / num2;
+  // return the result variable
+  return result;
+}
+
+function getPower(num1, num2) {
+  // create a result variable,
+  var result;
+  // assign it to num1 raised to the num2 power
+  result = num1 ** num2;
+  // return the result variable
+  return result;
+}
+
+function getRemainder(num1, num2) {
+  // create a result variable,
+  var result;
+  // assign it to remainder after dividing num1 by num2
+  result = num1 % num2;
+  // return the result variable
+  return result;
+}
+
+function increment(base, incrementer) {
+  // reassign base to result of incrementing base by incrementer
+  base += incrementer;
+  // return base
+  return base;
+}
+
+function decrement(base, decrementer) {
+  // reassign base to result of decrementing base by decrementer
+  base -= decrementer;
+  // return base
+  return base;
+}
+
+function applyTimesEquals(base, multiplier) {
+  // reassign base to result of multiplying base by multiplier
+  base *= multiplier;
+  // return base
+  return base;
+}
+
+function getAbsoluteValue(num) {
+  // create a result variable
+  var result;
+  // assign it to absolutely value of input num
+  result = Math.abs(num);
+  // return result
+  return result;
+}
+
+function roundDown(num) {
+  // create a result variable
+  var result;
+  // assign it to input, rounded down
+  result = Math.floor(num);
+  // return result
+  return result;
+}
