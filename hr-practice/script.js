87f2769eead590bc804373458cbe0faf722c9986
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-21 14:17:50 -0700

    if else with strings

--- hr-practice/script.js
+++ hr-practice/script.js
@@ -632,3 +632,15 @@ function balancePets(dogs, cats) {
   // otherwise
     // return 'number of cats or dogs is not acceptable'
 }
+
+{
+  // if password length is greater than 8
+  if (password.length > 8) {
+      return 'Password is long enough'
+  } else {
+      return 'Please enter a password of at least 9 characters';
+  }
+    // return 'Password is long enough'
+  // otherwise
+    // return 'Please enter a password of at least 9 characters'
+}
