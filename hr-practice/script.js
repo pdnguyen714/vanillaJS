7ffd5daebe0c72dfbfaa1b992715d53246348242
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-22 14:26:09 -0700

    if else if statements practice

--- hr-practice/script.js
+++ hr-practice/script.js
@@ -670,3 +670,23 @@ function measureRequiredFruit(fruitTotals) {
   // otherwise
     // return 'We do not have enough of both fruits'
 }
+
+function player1Choice(choice) {
+  // if player1 chose rock
+  if (choice === "rock") {
+      return "Player 1 chose rock";
+  } else if (choice === "paper") {
+      return "Player 1 chose paper";
+  } else if (choice === "scissors") {
+      return "Player 1 chose scissors";
+  } else {
+      return "Player 1 has chosen poorly!";
+  }
+    // return "Player 1 chose rock"
+  // otherwise if player1 chose paper
+    // return "Player 1 chose paper"
+  // otherwise if player1 chose scissors
+    // return "Player 1 chose scissors"
+  // otherwise
+    // return "Player 1 has chosen poorly!"
+}
