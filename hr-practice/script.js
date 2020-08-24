f5ad7f341a1941cde18be39bac44fd3dc66dc8be
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-24 16:29:26 -0700

    iterating through an array using continue to bypass specific index

--- hr-practice/script.js
+++ hr-practice/script.js
@@ -811,3 +811,16 @@ function loopInReverse(array) {
   }
     // log current array element to the console
 }
+
+function useContinue(array, index) {
+  // create a loop which iterates over the input array
+  for (var i = 0; i < array.length; i++) {
+        if (i === index) {
+            continue;
+        }
+    console.log(array[i]);
+  }
+    // if current index is equal to input index
+      // use described statement to skip to next iteration of loop (must include semi-colon!)
+    // log current array element to the console
+}
