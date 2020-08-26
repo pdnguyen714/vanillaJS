457b6939d48c64049adf8dadd30f48593015c424
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-26 09:47:38 -0700

    function to check if num is greater than 10 with boolean

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -7,68 +7,77 @@ function isOldEnoughToDrink(age) {
     }
   }
 
-  function isOldEnoughToDrive(age) {
+function isOldEnoughToDrive(age) {
+// your code here
+if (age >= 16) {
+    return true;
+} else {
+    return false;
+}
+}
+
+function isOldEnoughToVote(age) {
+// your code here
+if (age >= 18) {
+    return true;
+} else {
+    return false;
+}
+}
+
+function isOldEnoughToDrinkAndDrive(age) {
+// your code here
+if (age >= 21) {
+    return false;
+} else {
+    return false;
+}
+}
+
+function getProperty(obj, key) {
+// your code here
+return obj[key];
+}
+
+function addProperty(obj, key) {
+// your code here
+obj[key] = true;
+}
+
+function removeProperty(obj, key) {
+// your code here
+obj[key] = undefined;
+}
+
+function getFullName(firstName, lastName) {
+// your code here
+return firstName + " " + lastName;
+}
+
+function getLengthOfWord(word) {
+// your code here
+return word.length;
+}
+
+function getLengthOfTwoWords(word1, word2) {
+// your code here
+return word1.length + word2.length;
+}
+
+function checkAge(name, age) {
+// your code here
+if (age < 21) {
+    return "Go home, " + name + '!';
+} else {
+    return "Welcome, " + name + '!';
+}
+}
+
+function isGreaterThan10(num) {
     // your code here
-    if (age >= 16) {
+    if (num > 10) {
         return true;
     } else {
         return false;
-    }
-  }
-
-  function isOldEnoughToVote(age) {
-    // your code here
-    if (age >= 18) {
-        return true;
-    } else {
-        return false;
-    }
-  }
-
-  function isOldEnoughToDrinkAndDrive(age) {
-    // your code here
-    if (age >= 21) {
-        return false;
-    } else {
-        return false;
-    }
-  }
-
-  function getProperty(obj, key) {
-    // your code here
-    return obj[key];
-  }
-
-  function addProperty(obj, key) {
-    // your code here
-    obj[key] = true;
-  }
-
-  function removeProperty(obj, key) {
-    // your code here
-    obj[key] = undefined;
-  }
-
-  function getFullName(firstName, lastName) {
-    // your code here
-    return firstName + " " + lastName;
-  }
-
-  function getLengthOfWord(word) {
-    // your code here
-    return word.length;
-  }
-
-  function getLengthOfTwoWords(word1, word2) {
-    // your code here
-    return word1.length + word2.length;
-  }
-
-  function checkAge(name, age) {
-    // your code here
-    if (age < 21) {
-        return "Go home, " + name + '!';
-    } else {
-        return "Welcome, " + name + '!';
     }
   }
