470592d7be7eabc9ec8626eb0a075ecb83c79863
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-26 08:47:00 -0700

    function to find if old enough to drive

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -6,3 +6,12 @@ function isOldEnoughToDrink(age) {
         return false;
     }
   }
+
+  function isOldEnoughToDrive(age) {
+    // your code here
+    if (age >= 16) {
+        return true;
+    } else {
+        return false;
+    }
+  }
