24f300640c6bfaaaa03d5e5e235554ded7f736c2
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-27 20:30:38 -0700

    function that checks whether num1 or num2 are even

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -162,3 +162,12 @@ function equalsTen(num) {
         return false;
     }
   }
+
+  function isEitherEven(num1, num2) {
+    // your code here
+    if (num1 % 2 === 0 || num2 % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+  }
