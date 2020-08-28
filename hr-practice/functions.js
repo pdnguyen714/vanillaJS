7975f311a7687587c2398fcb2c8e09cf803589d6
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-27 20:16:57 -0700

    function to check whether word1 and word2 are the same length

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -144,3 +144,12 @@ function equalsTen(num) {
         return false;
     }
   }
+
+  function isSameLength(word1, word2) {
+    // your code here  
+    if (word1.length === word2.length) {
+        return true;
+    } else {
+        return false;
+    }
+  }
