81c3364dbc27e3905814576e25f520cb356f31d3
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-27 20:11:04 -0700

    function to check whether number is even

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -126,3 +126,12 @@ function equalsTen(num) {
         return false;
     }
   }
+
+  function isEven(num) {
+    // your code here
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+  }
