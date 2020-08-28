a8eeb678d9c6c534290a8b9dbaa650cf631ff60c
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-27 20:17:53 -0700

    function to check whether num1 and num2 are both odd

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -153,3 +153,12 @@ function equalsTen(num) {
         return false;
     }
   }
+
+  function areBothOdd(num1, num2) {
+    // your code here
+    if (num1 % 2 === 1 && num2 % 2 === 1) {
+        return true;
+    } else {
+        return false;
+    }
+  }
