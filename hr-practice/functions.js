55077d6a4efaa8283f9bda67ec1a0286234ce4dc
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-27 19:48:55 -0700

    function comparing whether num2 is less than num1

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -99,3 +99,12 @@ function equalsTen(num) {
         return false;
     }
   }
+
+  function isLessThan(num1, num2) {
+    // your code here
+    if (num1 > num2) {
+        return true;
+    } else {
+        return false;
+    }
+  }
