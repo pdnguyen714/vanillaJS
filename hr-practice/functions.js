87b6c1e09d29c169885eed7a9ceb5356b0dc44ab
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-27 19:53:52 -0700

    function that compares whether num1 and num2 are equal

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -117,3 +117,12 @@ function equalsTen(num) {
         return false;
     }
   }
+
+  function isEqualTo(num1, num2) {
+    // your code here
+    if (num1 === num2) {
+        return true;
+    } else {
+        return false;
+    }
+  }
