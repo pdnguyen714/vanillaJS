4d6f1bc68062aa445ae44e02ec001a0a3744dfad
Peter Nguyen <pdnguyen714@gmail.com>
2020-08-27 19:51:01 -0700

    function that compares whether num2 is greater than num1

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -108,3 +108,12 @@ function equalsTen(num) {
         return false;
     }
   }
+
+  function isGreaterThan(num1, num2) {
+    // your code here
+    if (num2 > num1) {
+        return true;
+    } else {
+        return false;
+    }
+  }
