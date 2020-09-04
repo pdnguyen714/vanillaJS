d2bb6e53f8e511662c0a1497f45fdb641a0b3dfb
Peter Nguyen <pdnguyen714@gmail.com>
2020-09-04 12:51:21 -0700

    function that returns the length of three words

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -337,3 +337,8 @@ function equalsTen(num) {
     // your code here
     return Math.sqrt(num) * 2;
   }
+
+  function getLengthOfThreeWords(word1, word2, word3) {
+    // your code here
+    return word1.length + word2.length + word3.length;
+  }
