bec2bc34e139073aef49e0294730f09fac713a82
Peter Nguyen <pdnguyen714@gmail.com>
2020-09-23 13:50:00 -0700

    function that removes numbers in object that are less than the given number

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -509,3 +509,12 @@ function equalsTen(num) {
         }
     }
   }
+
+  function removeNumbersLessThan(num, obj) {
+    // your code here
+    for (var key in obj) {
+        if (obj[key] < num) {
+            delete obj[key];
+        }
+    }
+  }
