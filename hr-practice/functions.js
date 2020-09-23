c2a85e6fbe08d9f6731e486913607b5b2e165481
Peter Nguyen <pdnguyen714@gmail.com>
2020-09-23 12:51:54 -0700

    function that seeks if obj contains array then removes it

--- hr-practice/functions.js
+++ hr-practice/functions.js
@@ -473,3 +473,12 @@ function equalsTen(num) {
         }
     }
   }
+
+  function removeArrayValues(obj) {
+    // your code here
+    for (var key in obj) {
+        if (Array.isArray(obj[key]) === true) {
+            delete obj[key];
+        }
+    }
+  }
